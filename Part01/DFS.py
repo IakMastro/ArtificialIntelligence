f2f7f1a765ad5b2c1d052b7823ac392c731908d3
IakMastro <cse242017102@uniwa.gr>
2020-10-25 19:02:54 +0200

    Shortest Path implemented

--- Part01/DFS.py
+++ Part01/DFS.py
@@ -2,27 +2,13 @@ from colors import *
 
 
 # Μέθοδος που υλοποιείται ο αλγόριθμος DFS σε Python
-def dfs(node, graph, found=False, visited=None):
-    if visited is None:
-        visited = []
-
-    # Εάν ο κόμβος δεν είναι στην λίστα visited (δηλαδή δεν το έχει επισκεφτεί ακόμα)
-    if node not in visited:
-        # Το προσθέτει στην λίστα από αυτών που τους έχει επισκεφτεί
-        visited.append(node)
-
-        if graph[node][0] == "EMPTY" and not found:
-            found = True
-
-        elif not found:
-            # Για κάθε γειτωνικό κόμβο που του κόμβου, ξανατρέξε το DFS με τον νέο κόμβο
-            for n in graph[node]:
-                if n[:1] == 'P':
-                    continue
-
-                dfs(n, graph, found, visited)
-
-        return visited
+def dfs(graph, node, goal, path=None):
+    if path is None:
+        path = [node]
+    if node == goal:
+        yield path
+    for next_node in graph[node] - set(path):
+        yield from dfs(graph, next_node, goal, path + [next_node])
 
 
 # Επείδηξη λειτουργίας του DFS αλγορίθμου
@@ -30,14 +16,24 @@ if __name__ == '__main__':
     print(GREEN + "Start of DFS Test" + DEFAULT)
 
     test_graph = {
-        '1': ["PLATFORM 1", '2', '4'],
-        '2': ["PLATFORM 2", '1', '3'],
-        '3': ["EMPTY", '2', '4'],
-        '4': ["PLATFORM 3", '1', '3']
+        '1': set(['2', '4']),
+        '2': set(['1', '3']),
+        '3': set(['2', '4']),
+        '4': set(['1', '3'])
     }
 
     for i in range(1, 5):
-        print(BLUE + f"{i}) Visited: {dfs(str(i), test_graph)}" + DEFAULT)
+        visited = list(dfs(test_graph, str(i), '3'))
+        print(BLUE + f"{i}) Visited: {visited}" + DEFAULT)
+
+        if len(visited) == 1:
+            visited = visited[0]
+        elif len(visited[0]) > len(visited[1]):
+            visited = visited[1]
+        else:
+            visited = visited[0]
+
+        print(BLUE + f"Fastest path: {visited}" + DEFAULT)
 
     print(GREEN + "Test was successful" + DEFAULT)
 
